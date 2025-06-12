dec9eac00fa73542cbba9364e1ba76e0fe0bb37e
Evolar <2729870067@qq.com>
2025-06-12 23:18:24 +0800

    example OK

--- User/example/example.c
+++ User/example/example.c
@@ -2,6 +2,7 @@
 // Created by w on 2025/6/12.
 //
 
+#include <malloc.h>
 #include "example.h"
 #include"motor_def.h"
 #include "motor_control.h"
@@ -12,7 +13,14 @@ static motor_config_t *example_motor_config;
 unitree_motor_object_t *example_motor_object;
 void init_example_motor()
 {
-    memset(example_motor_config,0,sizeof(motor_config_t));
+    //memset(example_motor_config,0,sizeof(motor_config_t));
+    // 为配置结构体分配内存
+    example_motor_config = (motor_config_t *)malloc(sizeof(motor_config_t));
+    if (example_motor_config == NULL) {
+        // 内存分配失败处理
+        return;
+    }
+    memset(example_motor_config, 0, sizeof(motor_config_t));
     example_motor_config->motor_type = GO_M8010;
     example_motor_config->cmd.channel = huart2;
     MOTOR_send *cmds=&example_motor_config->cmd;
@@ -23,6 +31,8 @@ void init_example_motor()
     cmds->Pos=0;
     cmds->K_P=0;
     cmds->K_W=0.05;
-    example_motor_object=unitree_motor_register(example_motor_config,NULL);
 
+    example_motor_object=unitree_motor_register(example_motor_config,NULL);
+    free(example_motor_config);
+    example_motor_config = NULL;
 }
